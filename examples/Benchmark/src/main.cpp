efcf150a8227f09251638646bb9bd56322b7edf6
alibzr <87379474+alibzr@users.noreply.github.com>
2024-04-16 19:09:56 +0200

    session priority edited

--- examples/Benchmark/src/main.cpp
+++ examples/Benchmark/src/main.cpp
@@ -199,7 +199,7 @@ int main(int argc, char** argv)
 
         // Initialize the varjo session
         varjo_Session* session = varjo_SessionInit();
-        varjo_SessionSetPriority(session, 1);
+        varjo_SessionSetPriority(session, 0);
 
         // Check if there was any errors while initializing.
         varjo_Error error = varjo_GetError(session);
