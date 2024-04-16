01780c646def85093fda94babce77c0b8a6cfdd4
Ali Bozorgian <87379474+alibzr@users.noreply.github.com>
2024-04-16 18:55:22 +0200

    Add project files.

---
+++ examples/build-vs-debug/cmake_install.cmake
@@ -0,0 +1,56 @@
+# Install script for directory: C:/Repository/varjo-sdk/examples
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Repository/varjo-sdk/examples/build-vs-debug")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("C:/Repository/varjo-sdk/examples/build-vs-debug/Benchmark/cmake_install.cmake")
+  include("C:/Repository/varjo-sdk/examples/build-vs-debug/EyeCameraStreamExample/cmake_install.cmake")
+  include("C:/Repository/varjo-sdk/examples/build-vs-debug/GazeTrackingExample/cmake_install.cmake")
+  include("C:/Repository/varjo-sdk/examples/build-vs-debug/MRExample/cmake_install.cmake")
+  include("C:/Repository/varjo-sdk/examples/build-vs-debug/MarkerExample/cmake_install.cmake")
+  include("C:/Repository/varjo-sdk/examples/build-vs-debug/ChromaKeyExample/cmake_install.cmake")
+  include("C:/Repository/varjo-sdk/examples/build-vs-debug/MaskingTool/cmake_install.cmake")
+
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "C:/Repository/varjo-sdk/examples/build-vs-debug/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
